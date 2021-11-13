bf78a887e1cfae4036169af699805ae1edb0cdbc
OndJJ <jcjk0302@gmail.com>
2021-11-13 17:17:36 +0900

    create 확인 필요

--- FLASKR/auth.py
+++ FLASKR/auth.py
@@ -28,7 +28,7 @@ def register():
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) BALUES (?,?)',
+                'INSERT INTO user (username, password) VALUES (?,?)',
                 (username, generate_password_hash(password))
             )
             db.commit()
